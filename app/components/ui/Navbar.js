6c2be7afed8fda99bbb02504d12952310ec9de7d
Jude Oscar <judeoscar2002gmail.com>
2023-11-23 10:55:44 +0100

    auth

--- app/components/ui/Navbar.js
+++ app/components/ui/Navbar.js
@@ -1,17 +1,21 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
+
 import logo from "../../../public/logo-text.svg";
 
 const Navbar = () => {
   return (
     <div>
       <div className="px-4 sm:px-8 py-4 flex justify-start bg-neutral-100 bg-opacity-50 backdrop-blur-[10px]">
-        <Image
-          src={logo}
-          alt="logo"
-          className=" w-[8.5rem] sm:w-auto h-10 sm:h-19"
-          priority={true}
-        />
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="logo"
+            className=" w-[8.5rem] sm:w-auto h-10 sm:h-19"
+            priority={true}
+          />
+        </Link>
       </div>
     </div>
   );

f51b36a90dfa2dc614744addde1b5e7f83f5a53f
drimescodes <olamidebamigbola@student.oauife.edu.ng>
2023-11-23 02:22:18 +0100

    Fixed the form in the login page

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -15,7 +15,40 @@ const page = () => {
             Login or create account
           </p>
 
-         
+          <form>
+            <div className="w-full mt-4">
+              <label htmlFor="email" className="text-black">
+                Email Address
+              </label>
+              <input
+                id="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#ffe08a]"
+                type="email"
+                placeholder="Email Address"
+                aria-label="Email Address"
+              />
+            </div>
+
+            <div className="w-full mt-4">
+              <label htmlFor="password" className="text-black">
+                Password
+              </label>
+              <input
+                id="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-[#ffe08a]"
+                type="password"
+                placeholder="Password"
+                aria-label="Password"
+              />
+            </div>
+
+            <button
+              className="w-full mt-4 px-6 py-2 text-sm font-medium tracking-wide text-gray-500 capitalize transition-colors duration-300 transform bg-[#ffe08a] rounded-lg hover:bg-[#FFB600] focus:outline-none focus:ring focus:ring-[#ffe08a] focus:ring-opacity-50"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </form>
         </div>
 
         <div className="flex items-center justify-center py-4 text-center bg-gray-50">

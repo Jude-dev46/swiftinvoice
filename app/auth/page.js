767e9c395b566a5f97bb8374ca6a0efbe83cd137
Jude Oscar <judeoscar2002gmail.com>
2023-12-15 13:04:55 +0100

    updated auth input

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,20 +1,22 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "../components/store/uislice";
-import Modal from "../components/ui/Modal";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Navbar from "../components/ui/Navbar";
+import Modal from "../components/ui/Modal";
 import * as yup from "yup";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 
 const Signup = () => {
   const dispatch = useDispatch();
   const [state, setState] = useState(0);
   const [message, setMessage] = useState("");
+  const [selectedValue, setSelectedValue] = useState();
   const router = useRouter();
 
   const isLoading = useSelector((state) => state.ui.isLoading);
@@ -54,6 +56,7 @@ const Signup = () => {
 
   const onSubmit = async (data, e) => {
     e.preventDefault();
+
     dispatch(uiActions.setIsLoading(true));
     try {
       const res = await axios.post("/api/auth", data);
@@ -70,164 +73,164 @@ const Signup = () => {
     }
   };
 
+  const handleSelectChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
   return (
-    <div className="h-[100vh] bg-gradient-to-br from-orange-100 via-red-100 to-violet-100 pt-28 w-full flex flex-col justify-center items-center">
-      {isLoading && <Modal />}
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col h-full px-12 py-4 w-full lg:w-1/3"
-      >
-        <div className="flex flex-row w-full space-x-3 mb-3">
-          <button
-            onClick={() => setState(0)}
-            className={
-              state === 0
-                ? "w-1/2 h-[5px] bg-yellow-500 rounded-md"
-                : "w-1/2 h-[5px] bg-neutral-200 rounded-md"
-            }
-          ></button>
-          <button
-            onClick={() => setState(1)}
-            className={
-              state === 1
-                ? "w-1/2 h-[5px] bg-yellow-500 rounded-md"
-                : "w-1/2 h-[5px] bg-neutral-200 rounded-md"
-            }
-          ></button>
-        </div>
-        {state === 0 && (
-          <div>
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="business" className="">
-                Business Name*
-              </label>
-              <input
-                type="text"
-                id="bname"
-                placeholder="e.g Benny's cakes"
-                className="bg-transparent border-2 border-black rounded-md outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
-                {...register("businessName")}
-              />
-            </div>
+    <>
+      <Navbar />
+      <div className="h-[100vh] bg-gradient-to-br from-orange-100 via-red-100 to-violet-100 pt-28 w-full flex flex-col justify-center items-center">
+        {isLoading && <Modal />}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col h-full px-12 py-4 w-full lg:w-1/3"
+        >
+          <div className="flex flex-row w-full space-x-3 mb-3">
+            <button
+              onClick={() => setState(0)}
+              className={
+                state === 0
+                  ? "w-1/2 h-[5px] bg-yellow-500 rounded-md"
+                  : "w-1/2 h-[5px] bg-neutral-200 rounded-md"
+              }
+            ></button>
+            <button
+              onClick={() => setState(1)}
+              className={
+                state === 1
+                  ? "w-1/2 h-[5px] bg-yellow-500 rounded-md"
+                  : "w-1/2 h-[5px] bg-neutral-200 rounded-md"
+              }
+            ></button>
+          </div>
+          {state === 0 && (
+            <div>
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
+                <label htmlFor="business" className="">
+                  Business Name*
+                </label>
+                <input
+                  type="text"
+                  id="bname"
+                  placeholder="e.g Benny's cakes"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
+                  {...register("businessName")}
+                />
+              </div>
 
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="business"> Business Field*</label>
-              <input
-                type="text"
-                id="bfield"
-                placeholder="e.g Cathering"
-                className="bg-transparent border-2 border-black rounded-md outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
-                {...register("businessField")}
-              />
-            </div>
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
+                <label htmlFor="business"> Business Field*</label>
+                <select
+                  id="bfield"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
+                  value={selectedValue}
+                  onChange={handleSelectChange}
+                  {...register("businessField")}
+                >
+                  <option value={"Accounting"}>Accounting</option>
+                  <option value={"Education"}>Education</option>
+                  <option value={"Finance"}>Finance</option>
+                  <option value={"Health Services"}>Health Services</option>
+                  <option value={"Human Resources"}>Human Resources</option>
+                  <option value={"IT Services"}>IT Services</option>
+                  <option value={"Sales & Marketing"}>Sales & Marketing</option>
+                </select>
+              </div>
 
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="dob">D.O.B*</label>
-              <input
-                type="date"
-                id="dob"
-                onInput={(e) => {
-                  e.target.value = e.target.value;
-                }}
-                className="bg-transparent text-center border-2 border-black rounded-md outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
-                {...register("date")}
-              />
-            </div>
-
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="business">Phone number* </label>
-              <div className="flex flex-row  border-2 border-black rounded-md ">
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
+                <label htmlFor="business">Phone number*</label>
                 <input
                   type="text"
                   id="number"
                   placeholder="+234 901 234 5678"
-                  className="w-full bg-transparent outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
                   {...register("phoneNo")}
                 />
               </div>
-            </div>
 
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="email">Email*</label>
-              <input
-                type="email"
-                id="email"
-                placeholder="abcd@gmail.com"
-                className="bg-transparent border-2 border-black rounded-md outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
-                {...register("email")}
-              />
-              <p className="text-black text-sm h-3 w-full">
-                *This is a required field
-              </p>
-            </div>
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
+                <label htmlFor="email">Email*</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="abcd@gmail.com"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
+                  {...register("email")}
+                />
+                {/* <p className="text-black text-sm h-3 w-full">
+                  *This is a required field
+                </p> */}
+              </div>
 
-            <div className="w-full">
-              <p
-                onClick={handleButton} // Make sure to define handleButton function
-                id="nextBtn"
-                className={
-                  state
-                    ? "w-full opacity-40 text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2 hover:cursor-pointer"
-                    : "w-full text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2 hover:cursor-pointer"
-                }
-              >
-                Next
-              </p>
-            </div>
-          </div>
-        )}
-        {state === 1 && (
-          <div>
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
-              <label htmlFor="password">Create Password*</label>
-              <input
-                type="password"
-                id="password"
-                className="bg-transparent border-2 border-black rounded-md outline-none px-2 py-1.5 lg:text-xl text-lg text-black"
-                {...register("password")}
-              />
-              <p className="text-black text-sm h-3 w-full">
-                Enter a mixture of letters, numbers, and symbols
-              </p>
-            </div>
-
-            <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-5">
-              <label htmlFor="confirm">Confirm Password*</label>
-              <input
-                type="password"
-                id="confirm"
-                className="bg-transparent border-2 border-black rounded-md outline-none px-2 py-1.5 text-lg lg:text-xl text-black"
-                {...register("confirm")}
-              />
-            </div>
-
-            <div className="w-full">
-              <button
-                type="submit"
-                className="w-full text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2"
-              >
-                Sign Up
-              </button>
-
-              <span className="flex flex-row space-x-1 justify-center items-center">
-                <p className="text-black text-sm lg:text-lg">
-                  Have an account?
+              <div className="w-full">
+                <p
+                  onClick={handleButton} // Make sure to define handleButton function
+                  id="nextBtn"
+                  className={
+                    state
+                      ? "w-full opacity-40 text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2 hover:cursor-pointer"
+                      : "w-full text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2 hover:cursor-pointer"
+                  }
+                >
+                  Next
                 </p>
-                <Link href="/auth/login">
-                  <p className="text-blue-700 text-sm lg:text-lg hover:cursor-pointer">
-                    Log in here
-                  </p>
-                </Link>
-              </span>
+              </div>
             </div>
-          </div>
-        )}
-        {message && <p className="text-red-600">{message}</p>}
-        {isError && (
-          <p className="text-red-600">An error occurred. Try again!</p>
-        )}
-      </form>
-    </div>
+          )}
+          {state === 1 && (
+            <div>
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-3">
+                <label htmlFor="password">Create Password*</label>
+                <input
+                  type="password"
+                  id="password"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
+                  {...register("password")}
+                />
+                <p className="text-black text-sm h-3 w-full">
+                  Enter a mixture of letters, numbers, and symbols
+                </p>
+              </div>
+
+              <div className="my-input flex flex-col w-full text-black text-lg lg:text-xl my-5">
+                <label htmlFor="confirm">Confirm Password*</label>
+                <input
+                  type="password"
+                  id="confirm"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg focus:border-[#FFB600] focus:ring-opacity-40 focus:outline-none  focus:ring-[#ffe08a]"
+                  {...register("confirm")}
+                />
+              </div>
+
+              <div className="w-full">
+                <button
+                  type="submit"
+                  className="w-full text-center text-black bg-yellow-500 px-10 lg:px-24 py-1.5 text-lg lg:text-xl lg:py-2 rounded-md mt-6 mb-2"
+                >
+                  Sign Up
+                </button>
+
+                <span className="flex flex-row space-x-1 justify-center items-center">
+                  <p className="text-black text-sm lg:text-lg">
+                    Have an account?
+                  </p>
+                  <Link href="/auth/login">
+                    <p className="text-blue-700 text-sm lg:text-lg hover:cursor-pointer">
+                      Log in here
+                    </p>
+                  </Link>
+                </span>
+              </div>
+            </div>
+          )}
+          {message && <p className="text-red-600">{message}</p>}
+          {isError && (
+            <p className="text-red-600">An error occurred. Try again!</p>
+          )}
+        </form>
+      </div>
+    </>
   );
 };
 
--- app/components/ui/Navbar.js
+++ app/components/ui/Navbar.js
@@ -5,15 +5,15 @@ import logo from "../../../public/logo-text.svg";
 const Navbar = () => {
   return (
     <div>
-       <Link href="/">
-        <div className="bg-neutral-100 bg-opacity-50 backdrop-blur-[10px] flex flex-row items-center fixed w-full px-5 py-3 lg:py-4 lg:px-8">
+      <div className="bg-neutral-100 bg-opacity-50 backdrop-blur-[10px] flex flex-row items-center fixed w-full px-5 py-3 lg:py-4 lg:px-8">
+        <Link href="/">
           <Image
             src={logo}
             alt=""
             className=" w-[14rem] lg:w-[20rem] lg:h-14 sm:w-auto h-10 sm:h-19"
           />
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 };
